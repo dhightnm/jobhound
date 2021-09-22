d8c176700f3f172253e690529f7894d398dd25bf
Devin Hight <dhightnm@gmail.com>
2021-09-22 15:58:28 -0700

    Conigure Basic Jest Tests

--- Spec/index.test.js
+++ Spec/index.test.js
@@ -1,3 +1,16 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import {cleanup, fireEvent, render} from '@testing-library/react';
+import App from '../client/src/app';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
 test('Simple test for CircleCI', () => {
   expect(2 + 2).toEqual(4);
+});
+
+it('Tests if App is properly rendering', () => {
+  shallow(<App/>);
 });
