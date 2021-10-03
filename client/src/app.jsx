b7daed04589142668df23c6942ba2d06a4b1817c
Devin Hight <dhightnm@gmail.com>
2021-10-02 21:40:55 -0700

    Formatted React Router

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -5,27 +5,27 @@ import NavBar from "./components/NavBar/NavBar";
 
 import Home from './components/Home/Home';
 import Login from "./components/authentication/login";
-import Navigation from "./Navigation";
+// import Navigation from "./Navigation";
 
 const App = (props) => {
 
   return (
     <>
-    <NavBar />
-    {/* <div>
-      This is the Home Screen
-    </div>
-    <a href="/login"><button >Login1</button> </a>
-    <AppliedTo /> */}
-    {/* <BrowserRouter>
+    <BrowserRouter>
     <div>
-      <Navigation />
+      
+    <NavBar />
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/login" component={Login} />
       </Switch>
     </div>
-    </BrowserRouter> */}
+    </BrowserRouter>
+    {/* <div>
+      This is the Home Screen
+    </div>
+    <a href="/login"><button >Login1</button> </a>
+    <AppliedTo /> */}
     </>
     
   )
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,25 +1,21 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import styles from '../../css/NavBar.module.css';
+import { NavLink } from 'react-router-dom';
+
 
 import Home from '../Home/Home';
 import Login from "../authentication/login";
-import Navigation from "../../Navigation";
 
 const NavBar = () => {
     return (
         <div className={styles.NavBar}>
-        <h1 >Welcome to Jobhound</h1> 
-    <BrowserRouter>
-        <div>
-        <Navigation />
-        <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/login" component={Login} />
-        </Switch>
-        </div>
-    </BrowserRouter>
-        </div>
+            <h1 >Welcome to Jobhound</h1> 
+            <div>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/login'> Login</NavLink>
+            </div>
+        </div> 
     )
 }
 export default NavBar;
